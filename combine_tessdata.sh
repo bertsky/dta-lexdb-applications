6c8721be536647157be9d6358742a619d97ce497
Robert Sachunsky <sachunsky@informatik.uni-leipzig.de>
2024-03-02 00:27:28 +0100

    add (reduced) number and punc DAWGs from langdata/deu

--- combine_tessdata.sh
+++ combine_tessdata.sh
@@ -2,4 +2,6 @@
 
 combine_tessdata -e $1 ${1%.traineddata}.lstm-unicharset
 wordlist2dawg $2 ${1%.traineddata}.lstm-word-dawg ${1%.traineddata}.lstm-unicharset
+wordlist2dawg deu.numbers ${1%.traineddata}.lstm-number-dawg ${1%.traineddata}.lstm-unicharset
+wordlist2dawg deu.punc ${1%.traineddata}.lstm-punc-dawg ${1%.traineddata}.lstm-unicharset
 combine_tessdata -o $1 ${1%.traineddata}.lstm-word-dawg
